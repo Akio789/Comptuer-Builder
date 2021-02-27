df3e2a13f35a4ce60f4aff4cac1a6e0198661180
Jorge Figueroa Peart <54185889+JorgeFigueroaPeart@users.noreply.github.com>
2021-02-26 21:47:24 -0600

    Better ui in login and sign up

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,61 +1,9 @@
-@extends('layouts.main')
-@section('content')
-
-<!------ Include the above in your HEAD tag ---------->
-
 <!DOCTYPE html>
 <html>
-<head>
-    
-    <!--Fontawesome CDN-->
+	<head>
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-
-	<!--Custom styles-->
-	<link rel="stylesheet" type="stylesheet" href="{{URL::asset('/css/login.css')}}" integrity="">
-</head>
-<body>
-<div class="container">
-	<div class="d-flex justify-content-center h-100">
-		<div class="card">
-			<div class="card-header">
-				<h3>Sign In</h3>
-				<div class="d-flex justify-content-end social_icon">
-					<span><i class="fab fa-facebook-square"></i></span>
-					<span><i class="fab fa-google-plus-square"></i></span>
-				</div>
-			</div>
-			<div class="card-body">
-				<form action="/login" method="POST">
-                @csrf
-					<div class="input-group form-group">
-						<div class="input-group-prepend">
-							<span class="input-group-text"><i class="fas fa-user"></i></span>
-						</div>
-						<input name="email" id="email" type="text" class="form-control" placeholder="email">
-						
-					</div>
-					<div class="input-group form-group">
-						<div class="input-group-prepend">
-							<span class="input-group-text"><i class="fas fa-key"></i></span>
-						</div>
-						<input name="password" id="password" type="password" class="form-control" placeholder="password">
-					</div>
-					<div class="form-group">
-						<input type="submit" value="Login" class="btn float-right login_btn">
-					</div>
-				</form>
-			</div>
-			<div class="card-footer">
-                @if (session('failed'))
-                    <p>Incorrect credentials</p>
-                @endif
-				<div class="d-flex justify-content-center links">
-					Don't have an account?<a href="{{ route('users.create') }}">Sign Up</a>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-</body>
+	</head>
+	<body>
+		@include('users.login')
+	</body>
 </html>
-@endsection
--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <!--Bootsrap 4 CDN-->
+	<title>Login</title>
+	<!--Bootsrap 4 CDN-->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     
     <!--Fontawesome CDN-->
@@ -9,30 +10,29 @@
 
 	<!--Custom styles-->
 	<link rel="stylesheet" type="text/css" href="{{URL::asset('/css/main.css')}}">
+
+    
 </head>
 <body>
 <div class="container">
 	<div class="d-flex justify-content-center h-100">
 		<div class="card">
 			<div class="card-header">
-				<h3>Create new account</h3>
+				<h3>Sign In</h3>
 				<div class="d-flex justify-content-end social_icon">
+					<span><i class="fab fa-facebook-square"></i></span>
+					<span><i class="fab fa-google-plus-square"></i></span>
 				</div>
 			</div>
 			<div class="card-body">
-				<form action="{{ route('users.store') }}" method="POST">
+				<form action="/login" method="POST">
                 @csrf
-                    <div class="input-group form-group">
+					<div class="input-group form-group">
 						<div class="input-group-prepend">
 							<span class="input-group-text"><i class="fas fa-user"></i></span>
 						</div>
-						<input name="name" id="name" type="text" class="form-control" placeholder="name">
-					</div>
-					<div class="input-group form-group">
-						<div class="input-group-prepend">
-							<span class="input-group-text"><i class="fas fa-envelope"></i></span>
-						</div>
 						<input name="email" id="email" type="text" class="form-control" placeholder="email">
+						
 					</div>
 					<div class="input-group form-group">
 						<div class="input-group-prepend">
@@ -41,15 +41,17 @@
 						<input name="password" id="password" type="password" class="form-control" placeholder="password">
 					</div>
 					<div class="form-group">
-						<input type="submit" value="Register" class="btn float-right login_btn">
+						<input type="submit" value="Login" class="btn float-right login_btn">
 					</div>
 				</form>
 			</div>
 			<div class="card-footer">
+				<div class="d-flex justify-content-center links">
+					Don't have an account?<a href="{{ route('users.create') }}">Sign Up</a>
+				</div>
 			</div>
 		</div>
 	</div>
 </div>
-
 </body>
 </html>
