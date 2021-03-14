1952e577b08d111f5070ff2a6a7a5fef93a8101f
Akio Olvera <akiooarao@gmail.com>
2021-03-14 13:31:00 -0700

    Missing functions in AuthenticationController

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+
+use App\Models\User;
 
 class AuthenticationController extends Controller
 {
@@ -38,5 +42,37 @@ class AuthenticationController extends Controller
         }
 
         return redirect()->back()->withErrors(['Incorrect credentials']);
+    }
+
+    /**
+     * Show register form.
+     */
+    public function registerForm(Request $request)
+    {
+        return view('users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $data = $request->input();
+
+        Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email:rfc,dns|unique:users',
+            'password' => 'required|confirmed'
+        ])->validate();
+
+        $data['password'] = Hash::make($data['password']);
+
+        $user = User::create($data);
+        Auth::login($user);
+
+        return redirect()->route('computers.index');
     }
 }
